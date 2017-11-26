c66919acc72e1ada5efe0bb784cefb2a73773303
Ken Coppola <mwave317@gmail.com>
2017-11-26 01:42:14 -0500

    Added Logout Button

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class Header extends Component {
+
+  logOut() {
+  fetch('https://desolate-lowlands-68945.herokuapp.com/logout', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    },
+  })
+};
+
+
   render() {
     return (
       <div className="header">
         <p className="headerPtag">Fork in the Road</p>
-    
+        <Link to='/'><button onClick={() => this.logOut()}>Log Out</button></Link>
       </div>
     );
   }
